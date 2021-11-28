eb138c98b115a020afb78409459774a7360ce32e
weltonsousa <weltonsousa@gmail.com>
2021-11-28 10:17:13 -0300

    Simple create form

--- SalesWeb/Controllers/SellersController.cs
+++ SalesWeb/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Models;
 using SalesWeb.Services;
 using System;
 using System.Collections.Generic;
@@ -22,6 +23,19 @@ namespace SalesWeb.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost] //obtenm do  resquest
+        [ValidateAntiForgeryToken] // validacao contra ataque crsf
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
