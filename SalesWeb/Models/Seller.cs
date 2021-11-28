cb41f0ee4f6a2883bab2e76370758c7ee5e7b6e4
weltonsousa <weltonsousa@gmail.com>
2021-11-28 11:11:15 -0300

    Foreign key not null (referential integrity)

--- SalesWeb/Models/Seller.cs
+++ SalesWeb/Models/Seller.cs
@@ -17,6 +17,8 @@ namespace SalesWeb.Models
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
+
+        public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
