8b6bda5c14a5aeabade596d8bd4a828a025d28d2
weltonsousa <weltonsousa@gmail.com>
2021-11-29 23:31:11 -0300

    Seller details and eager loading

--- SalesWeb/Services/SellerService.cs
+++ SalesWeb/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWeb.Models;
 using SalesWeb.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWeb.Services
 {
@@ -31,7 +32,8 @@ namespace SalesWeb.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            //Includ() eager loading faz um relacionamento entre tabelas
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
